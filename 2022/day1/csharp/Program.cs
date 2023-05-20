24bb4488cbf68e337a1eacd182c12c937df4b6a5
r3d5un <r3d5un@pm.me>
2023-05-20 16:07:12 +0200

    Day 1 - C#

--- 2022/day1/csharp/Program.cs
+++ 2022/day1/csharp/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿String inputText = File.ReadAllText("/home/r3d5un/Development/Learning/adventofcode/2022/day1/input.txt");
 
-Console.WriteLine("Hello, World!");
+string[] calorieGroupArray = inputText.Split("\n\n");
+
+List<int> caloriesPerElfList = new List<int> { };
+
+foreach (String group in calorieGroupArray) {
+    if (string.IsNullOrWhiteSpace(group))
+        continue;
+
+    List<String> calorieNumberList = new List<string>(group.Split("\n").ToList());
+
+    int currentElfCalories = 0;
+    foreach (String calorieString in calorieNumberList)
+    {
+        currentElfCalories += Convert.ToInt32(calorieString);
+    }
+
+    caloriesPerElfList.Add(currentElfCalories);
+}
+
+// Answer to Part 1
+Console.WriteLine(caloriesPerElfList.Max());
+
+// Answert to Part 2
+int topThreeCalories = (from i in caloriesPerElfList
+    orderby i descending
+    select i).Take(3).Sum();
+Console.WriteLine(topThreeCalories);
