63e2c3140db930c59c0dd0dcf0d05ea36b1c0a48
r3d5un <r3d5un@pm.me>
2023-05-23 19:23:39 +0200

    Simplify control flow

--- 2022/day1/rust/src/main.rs
+++ 2022/day1/rust/src/main.rs
@@ -3,21 +3,19 @@ use std::fs;
 fn main() {
     let input_text = fs::read_to_string("../input.txt").expect("Failed to read the file.");
     let calorie_groups: Vec<&str> = input_text.split("\n\n").collect();
-    // dbg!(calorie_groups);
 
     let mut calories_per_elf: Vec<i32> = Vec::new();
     for group in calorie_groups {
-        if group.is_empty() {
-            continue;
-        }
-        let calorie_numbers_for_elf: Vec<&str> = group.split("\n").collect();
+        if !group.is_empty() {
+            let calorie_numbers_for_elf: Vec<&str> = group.split("\n").collect();
 
-        let mut current_elf_sum: i32 = 0;
-        for number in calorie_numbers_for_elf {
-            let parsed_number = number.parse::<i32>().unwrap();
-            current_elf_sum += parsed_number;
+            let mut current_elf_sum: i32 = 0;
+            for number in calorie_numbers_for_elf {
+                let parsed_number = number.parse::<i32>().unwrap();
+                current_elf_sum += parsed_number;
+            }
+            calories_per_elf.push(current_elf_sum);
         }
-        calories_per_elf.push(current_elf_sum);
     }
     let highest_number_of_calories = calories_per_elf.iter().max().unwrap();
 
