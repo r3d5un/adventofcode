e08a94bc8643343fb4b4690b2e7089fe39751fed
r3d5un <r3d5un@pm.me>
2023-05-30 21:55:28 +0200

    It runs, but is wrong
    :(

---
+++ 2022/day3/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using static csharp.Toolbox;
+
+IEnumerable<String> inputText = File.ReadLines(
+    "/home/r3d5un/Development/Learning/adventofcode/2022/day3/input.txt"
+);
+
+int sumPartOne = 0;
+foreach (String line in inputText)
+{
+    List<List<char>> compartmentLists = SplitIntoCompartments(line);
+    char item = GetIntersectionCharacter(compartmentLists[0], compartmentLists[1]);
+    sumPartOne += ConvertToPriorityValue(item);
+}
+Console.WriteLine($"Part one sum: {sumPartOne}");
