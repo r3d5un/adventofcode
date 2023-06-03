a0ba9f0e712c90eb827b25366cba43b90a5ec763
r3d5un <r3d5un@pm.me>
2023-06-03 15:53:11 +0200

    Add convert_to_priority_value tests

--- 2022/day3/rust/src/main.rs
+++ 2022/day3/rust/src/main.rs
@@ -11,3 +11,21 @@ fn convert_to_priority_value(character: &char) -> u32 {
     let initial_lowercase_vaue: u32 = 'a'.into();
     return character_value - initial_lowercase_vaue + 1;
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_to_priority_value_uppercase() {
+        let character: char = 'a';
+        let expected_value: u32 = 1;
+        assert_eq!(expected_value, convert_to_priority_value(&character));
+    }
+
+    #[test]
+    fn test_convert_to_priority_value_lowercase() {
+        let character: char = 'A';
+        let expected_value: u32 = 27;
+        assert_eq!(expected_value, convert_to_priority_value(&character));
+    }
+}
