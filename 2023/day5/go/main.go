e898e4abcef9d4dfbf40edc84f197c87e2d570cc
r3d5un <r3d5un@pm.me>
2023-12-09 19:28:05 +0100

    Parse mappings

--- 2023/day5/go/main.go
+++ 2023/day5/go/main.go
@@ -20,6 +20,9 @@ func main() {
 func Part1(input string) {
 	seeds := ParseSeeds(input)
 	fmt.Println(seeds)
+
+	SeedToSoil := ParseMapping(input, "seed-to-soil")
+	fmt.Println(SeedToSoil)
 }
 
 func ParseSeeds(input string) (seeds []int) {
@@ -42,4 +45,61 @@ func ParseSeeds(input string) (seeds []int) {
 	}
 
 	return seeds
+}
+
+func ParseMapping(input string, mapping string) (ranges []Range) {
+	var values []string
+	takeNext := false
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		if strings.Contains(line, mapping) {
+			takeNext = true
+			continue
+		}
+		if line == "" && takeNext {
+			break
+		}
+		if takeNext {
+			values = append(values, line)
+		}
+	}
+
+	for _, value := range values {
+		ranges = append(ranges, ParseRange(value))
+	}
+
+	return ranges
+}
+
+func ParseRange(value string) Range {
+	mapValues := strings.Fields(value)
+	fmt.Println(mapValues)
+
+	destinationRangeStart, err := strconv.Atoi(mapValues[0])
+	if err != nil {
+		panic(err)
+	}
+
+	sourceRangeStart, err := strconv.Atoi(mapValues[1])
+	if err != nil {
+		panic(err)
+	}
+
+	rangeLength, err := strconv.Atoi(mapValues[2])
+	if err != nil {
+		panic(err)
+	}
+
+	return Range{
+		DestinationRangeStart: destinationRangeStart,
+		SourceRangeStart:      sourceRangeStart,
+		RangeLength:           rangeLength,
+	}
+}
+
+type Range struct {
+	DestinationRangeStart int
+	SourceRangeStart      int
+	RangeLength           int
 }
